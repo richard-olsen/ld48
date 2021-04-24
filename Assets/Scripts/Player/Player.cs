d1040694d598eb34846c2287b66a7983567f2abd
Richard Olsen <olsen7781@gmail.com>
2021-04-23 23:50:00 -0600

    Player snaps to grid when level starts

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
     private int positionX = 0;
-    
-    [SerializeField]
     private int positionY = 0;
 
-    [SerializeField]
     private bool canMoveX = true;
-    [SerializeField]
     private bool canMoveY = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        MoveAlongGrid(0, 0);
+        SnapToGrid();
     }
 
     private void FixedUpdate()
@@ -63,6 +58,15 @@ public class Player : MonoBehaviour
         
     }
 
+    public void SnapToGrid()
+    {
+        int x = Mathf.RoundToInt(transform.position.x - 0.5f);
+        int y = Mathf.RoundToInt(transform.position.y - 0.5f);
+
+        MoveAlongX(x);
+        MoveAlongY(y);
+    }
+
     public void MoveAlongX(int xOffset)
     {
         Vector3 position = transform.position;
@@ -88,28 +92,29 @@ public class Player : MonoBehaviour
     // Returns true if movement is successful
     public bool MoveAlongGrid(int xOffset, int yOffset)
     {
+        if (xOffset == 0 && yOffset == 0)
+            return false;
+
         // Do some ray casting to see if entity can move into slot
         // Try to move to another slot if possible
-        if (xOffset != 0 || yOffset != 0)
+        // Assuming everything is a bounding box filling the entire single grid space
+        RaycastHit2D ray = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(xOffset, yOffset).normalized, 1.0f);
+
+        if (ray.collider != null) // Something exists in the targeted slot
         {
-            // Assuming everything is a bounding box filling the entire single grid space
-            RaycastHit2D ray = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(xOffset, yOffset).normalized, 1.0f);
+            // Try along 2 other axis
 
-            if (ray.collider != null)
-            {
-                // Try along 2 other axis
-
-                if (xOffset == 0 || yOffset == 0)
-                    return false; // No where else to try
-
-                if (MoveAlongGrid(xOffset, 0))
-                    return true;
-                if (MoveAlongGrid(0, yOffset))
-                    return true;
-
+            if (xOffset == 0 || yOffset == 0)
                 return false; // No where else to try
-            }
+
+            if (MoveAlongGrid(xOffset, 0))
+                return true;
+            if (MoveAlongGrid(0, yOffset))
+                return true;
+
+            return false; // No where else to try
         }
+
         MoveAlongX(xOffset);
         MoveAlongY(yOffset);
 
