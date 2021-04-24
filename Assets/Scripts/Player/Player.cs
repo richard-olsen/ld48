ec592bdc70ed0d6d98b529fa770a3dc0fa77802e
Richard Olsen <olsen7781@gmail.com>
2021-04-23 23:38:23 -0600

    Player collides with walls

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,11 +6,9 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private int positionX = 0;
-    private int prevPositionX = 0;
     
     [SerializeField]
     private int positionY = 0;
-    private int prevPositionY = 0;
 
     [SerializeField]
     private bool canMoveX = true;
@@ -20,7 +18,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAlongGrid(0, 0);
     }
 
     private void FixedUpdate()
@@ -28,21 +26,24 @@ public class Player : MonoBehaviour
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
+        int moveX = 0;
+        int moveY = 0;
+
         if (canMoveX)
         {
             if (hor < 0)
-                positionX--;
+                moveX--;
             if (hor > 0)
-                positionX++;
+                moveX++;
 
             canMoveX = false;
         }
         if (canMoveY)
         {
             if (ver < 0)
-                positionY--;
+                moveY--;
             if (ver > 0)
-                positionY++;
+                moveY++;
 
             canMoveY = false;
         }
@@ -51,30 +52,67 @@ public class Player : MonoBehaviour
             canMoveX = true;
         if (ver == 0)
             canMoveY = true;
+
+        if (moveX != 0 || moveY != 0)
+            MoveAlongGrid(moveX, moveY);
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool changed = false;
+        
+    }
 
-        if (positionX != prevPositionX)
-            changed = true;
+    public void MoveAlongX(int xOffset)
+    {
+        Vector3 position = transform.position;
 
-        if (positionY != prevPositionY)
-            changed = true;
+        positionX += xOffset;
 
-        if (changed)
+        position.x = (float)positionX + 0.5f;
+
+        transform.position = position;
+    }
+
+    public void MoveAlongY(int yOffset)
+    {
+        Vector3 position = transform.position;
+
+        positionY += yOffset;
+
+        position.y = (float)positionY + 0.5f;
+
+        transform.position = position;
+    }
+
+    // Returns true if movement is successful
+    public bool MoveAlongGrid(int xOffset, int yOffset)
+    {
+        // Do some ray casting to see if entity can move into slot
+        // Try to move to another slot if possible
+        if (xOffset != 0 || yOffset != 0)
         {
-            Vector3 position = transform.position;
+            // Assuming everything is a bounding box filling the entire single grid space
+            RaycastHit2D ray = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(xOffset, yOffset).normalized, 1.0f);
 
-            position.x = (float)positionX + 0.5f;
-            position.y = (float)positionY + 0.5f;
+            if (ray.collider != null)
+            {
+                // Try along 2 other axis
 
-            prevPositionX = positionX;
-            prevPositionY = positionY;
+                if (xOffset == 0 || yOffset == 0)
+                    return false; // No where else to try
 
-            transform.position = position;
+                if (MoveAlongGrid(xOffset, 0))
+                    return true;
+                if (MoveAlongGrid(0, yOffset))
+                    return true;
+
+                return false; // No where else to try
+            }
         }
+        MoveAlongX(xOffset);
+        MoveAlongY(yOffset);
+
+        return true;
     }
 }
