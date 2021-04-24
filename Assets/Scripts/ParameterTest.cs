e823d4993b27c21989e31cbd005ccd48a4ae471d
Technostalgic <tteck02@gmail.com>
2021-04-23 18:25:58 -0600

    added some test prefabs

---
+++ Assets/Scripts/ParameterTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterTest : MonoBehaviour
+{
+    [SerializeField]
+    private TestScript _test;
+
+    [SerializeField]
+    private float _health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
