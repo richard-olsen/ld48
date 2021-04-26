dfb4a67266b1affda6c718f048d9c631f2a7aa0c
Technostalgic <tteck02@gmail.com>
2021-04-26 01:38:11 -0600

    hazards are now destroyed when out of camera view

--- Assets/LevelAsset/Hazards/Hazard.cs
+++ Assets/LevelAsset/Hazards/Hazard.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 min = Camera.main.ViewportToWorldPoint(Camera.main.rect.min);
+        Vector3 max = Camera.main.ViewportToWorldPoint(Camera.main.rect.max);
+
+        // get the camera viewport in world coords and expand it by one in each direction
+        Rect vp = new Rect(min, max - min);
+        vp.position -= new Vector2(2, 2);
+        vp.size += new Vector2(1, 1);
+
+        // if the object falls outside the camera viewport
+		if (!vp.Contains(transform.position))
+		{
+            gameObject.SetActive(false);
+		}
     }
 }
