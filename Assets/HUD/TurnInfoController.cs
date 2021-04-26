229d27fecba65fb88f80a4493e07bf2518ddc70b
Technostalgic <tteck02@gmail.com>
2021-04-26 02:04:47 -0600

    made HUD display player turns

---
+++ Assets/HUD/TurnInfoController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TurnInfoController : MonoBehaviour
+{
+	[SerializeField]
+	private HUDController _hud;
+
+	[SerializeField]
+	private TextMeshProUGUI _turnStateText;
+
+	[SerializeField]
+	private TextMeshProUGUI _turnCountText;
+
+	private Color _oCol;
+	private string playerTurnText = "Your Turn";
+	private string enemyTurnText = "Enemy's Turn";
+
+	private TurnBasedMovementSystem _turnSystem;
+	public TurnBasedMovementSystem TurnSystem => _turnSystem ?? (_turnSystem = FindObjectOfType<TurnBasedMovementSystem>());
+
+	private void OnEnable()
+	{
+		_oCol = _turnStateText.color;
+		_turnSystem = FindObjectOfType<TurnBasedMovementSystem>();
+	}
+
+	private void Update()
+	{
+		_turnCountText.text = TurnSystem.PlayerActionsLeft.ToString();
+
+		// player turn
+		if (TurnSystem.PlayerActionsLeft > 0)
+		{
+			_turnStateText.color = _oCol; ;
+			_turnStateText.text = playerTurnText;
+		}
+
+		// enemy turn
+		else
+		{
+			_turnStateText.color = Color.gray;
+			_turnStateText.text = enemyTurnText;
+		}
+	}
+}
